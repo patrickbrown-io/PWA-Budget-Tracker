004022a12ce63030467e46ded849b9fc9b0b22a8
Patrick Brown <breepatbree@gmail.com>
2021-09-26 19:59:35 -0600

    PWA download seems to work

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,8 +10,8 @@ const FILES_TO_CACHE = [
 ];
 
 // Variables to store cache
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v3";
+const DATA_CACHE_NAME = "data-cache-v2";
 
 //  Install
 self.addEventListener("install", function (event) {
